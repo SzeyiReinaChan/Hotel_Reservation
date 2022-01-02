5be416dd3c64afbb54baa27cac20fda11550a182
SzeyiReinaChan <szeyic@gmail.com>
2022-01-02 18:06:45 -0500

    Update CustomerService.java

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -29,7 +29,6 @@ public class CustomerService {
             if (customerEmail.equals(currentCustomer.getEmail())){
                 return currentCustomer;
             }
-//            System.out.println("Customer does not exist.");
             return null;
         }
         return null;
