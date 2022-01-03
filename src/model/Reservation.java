60e5db8cfbab0fc6b456f821d1d732ddbb1a84f3
SzeyiReinaChan <szeyic@gmail.com>
2022-01-03 15:53:44 -0500

    Update Reservation.java

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -2,6 +2,7 @@ package model;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class Reservation {
     private Customer customer;
@@ -20,22 +21,55 @@ public class Reservation {
         return customer;
     }
 
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
     public IRoom getRoom() {
         return room;
+    }
+
+    public void setRoom(IRoom room) {
+        this.room = room;
     }
 
     public Date getCheckInDate() {
         return checkInDate;
     }
 
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
     public Date getCheckOutDate() {
         return checkOutDate;
     }
 
-    public String toString(){
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public String toString() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy");
         return "Customer Info: " + customer + "\nRoom Info: " + room +
                 "\nCheck In Date : " + simpleDateFormat.format(checkInDate) +
                 "\nCheck Out Date: " + simpleDateFormat.format(checkOutDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Reservation)) return false;
+        Reservation that = (Reservation) o;
+        return customer.equals(that.customer)
+                && room.equals(that.room)
+                && checkInDate.equals(that.checkInDate)
+                && checkOutDate.equals(that.checkOutDate);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(getCustomer(), getRoom(), getCheckInDate(), getCheckOutDate());
     }
 }
